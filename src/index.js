57e4f1baab9500f19c79d4aab258a06b744d0838
muramasah <hitokiri.muramasa@gmail.com>
2017-12-24 13:29:25 -0300

    add factory methods

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import Scraper from './scraper/Scraper';
-import { CheerioEngine as Cheerio } from './engine/CheerioEngine';
-import { RegexEngine as Regex } from './engine/RegexEngine'
-import { EngineInterface as Engine } from './engine/EngineInterface';
 
-export let CheerioEngine = Cherio;
-export let RegexEngine = Regex;
+import {
+    createScraper as create
+} from './scraper/Scraper';
+import {
+    CheerioEngine as Cheerio
+} from './engine/CheerioEngine';
+import {
+    RegexEngine as Regex
+} from './engine/RegexEngine'
+import {
+    EngineInterface as Engine
+} from './engine/EngineInterface';
+
 export let EngineInterface = Engine;
+export let CheerioEngine = Cheerio;
+export let RegexEngine = Regex;
+
+export const createScraper = create;
+
+export const createCheerioScraper = (extractors, html) => {
+    return createScraper(extractors, CheerioEngine, html);
+}
+
+export const createRegexScraper = (extractors, html) => {
+    return createScraper(extractors, RegexEngine, html);
+}
+
 export default Scraper;
