633e24b0c7b2da71e4cd52933583c85c4a2d55fa
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-07-19 11:22:58 -0300

    Example created

---
+++ example/movieScraper.js
@@ -0,0 +1,26 @@
+// THIRD PARTY DEPENDENCIES
+const axios = require('axios');
+
+// INTERNAL DEPENDENCIES
+const { createScraper, CheerioEngine } = require('../index');
+
+// Data extractors
+const dataMovieCheerioExtractors = {
+  title: ($) => $('.title_wrapper').find('h1').text(),
+  release: ($) => $('.summary_text').text(),
+  rating: ($) => $('.ratingValue').find('strong').find('span').text()
+};
+
+// Get the website
+axios.get('http://www.imdb.com/title/tt1229340/')
+  .then(({ data: html }) => {
+    if (html) {
+      // Create scraper
+      const dataMovieCheerioScraper = createScraper(dataMovieCheerioExtractors, CheerioEngine, html);
+      // Scrap
+      const extractedData = dataMovieCheerioScraper.scrapLoadedHtml();
+      // Show data
+      console.log({ extractedData });
+    }
+  });
+  
