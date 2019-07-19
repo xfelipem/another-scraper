717575f2c3e8a61a91014556c9387df1e6a5cb21
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-07-19 16:27:09 -0300

    Added Regex example

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 // INTERNAL DEPENDENCIES
-const { createScraper, CheerioEngine } = require('../index');
+const { createCheerioScraper, createRegexScraper } = require('../index');
 
 // Data extractors
 const dataMovieCheerioExtractors = {
@@ -10,19 +10,22 @@ const dataMovieCheerioExtractors = {
   release: ($) => $('.summary_text').text(),
   rating: ($) => $('.ratingValue').find('strong').find('span').text()
 };
-
+const dataUrlsRegexpExtractors = {
+  urls: (html) => html.match(/(https?:\/\/[^\s]+)/g)
+};
 // Get the website
 axios.get('http://www.imdb.com/title/tt1229340/')
   .then((response) => {
     const { data: html } = response;
-    
-    console.log(response);
 
     if (html) {
       // Create scraper
-      const dataMovieCheerioScraper = createScraper(dataMovieCheerioExtractors, CheerioEngine, html);
+      const dataMovieCheerioScraper = createCheerioScraper(dataMovieCheerioExtractors, html);
+      const dataUrlsRegexpScraper = createRegexScraper(dataUrlsRegexpExtractors, html);
       // Scrap
-      const extractedData = dataMovieCheerioScraper.scrapLoadedHtml();
+      const extractedData = {
+        ...dataMovieCheerioScraper.scrapLoadedHtml(), ...dataUrlsRegexpScraper.scrapLoadedHtml()
+      };
       // Show data
       console.log({ extractedData });
     }
