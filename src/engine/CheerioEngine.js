9c15f552caceac78f9e7e20a2772adb6f41d5b22
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-07-19 15:41:58 -0300

    Refactored data iteration

--- src/engine/CheerioEngine.js
+++ src/engine/CheerioEngine.js
@@ -1,23 +1,18 @@
+// THIRD PARTY DEPENDENCIES
 import cheerio from 'cheerio';
+
+// INTERNAL DEPENDENCIES
 import Engine from './EngineInterface';
-const forEach = function (object, method) {
-    Object.keys(object).forEach(function (key) {
-        let parameter = object[key];
-
-        method(parameter, key, object);
-    });
-
-    return object;
-}
 
 export class CheerioEngine extends Engine {
     extractData(html, dataExtractors) {
         const $html = cheerio.load(html);
         let extractedData = {};
 
-        forEach(dataExtractors, (extractor, dataTitle) => {
-            //console.log('forEach CB', { extractor, title });
-            extractedData[dataTitle] = extractor($html)
+        Object.keys(dataExtractors).forEach((name) => {
+            const extractor = dataExtractors[name];
+
+            extractedData[name] = extractor($html)
         });
 
         return extractedData;
